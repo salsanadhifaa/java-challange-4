7e86dc6879384e17b254074bae89ad30891c0271
salsanadhifaa <91627039+salsanadhifaa@users.noreply.github.com>
2021-10-19 00:25:32 +0700

    Add files via upload

---
+++ JavaMethod.java
@@ -0,0 +1,20 @@
+public class JavaMethod{
+    public static void main(String[] args) {
+        human("Salsa",21);
+    }
+
+    static void human(String nama, int umur) {
+        System.out.println("Nama :" + nama +",umur :" +umur);
+
+
+    myMethod("Salsa Nadhifa","Universitas Muhammadiyah Sumatra Utara","Group9");
+    }
+    static void myMethod(String nama,String kampus,String anggota){
+        System.out.println("Nama:" + nama);
+        System.out.println("Kampus :" + kampus);
+        System.out.println("Group :" + anggota);
+    }
+
+
+
+}
